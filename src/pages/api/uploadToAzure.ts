d0180f699cfa2867802a5695636a0cbb12694d6d
joohyun97 <jpark97@vt.edu>
2024-11-04 23:06:37 -0500

    Initialized File Paths and Main Page

---
+++ src/pages/api/uploadToAzure.ts
@@ -0,0 +1,7 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+import { BlobServiceClient } from '@azure/storage-blob';
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse)
+{
+    
+}
